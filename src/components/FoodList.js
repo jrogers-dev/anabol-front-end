63dec3e7a507f3ada004671f3443c3887a8135b6
jrogers-dev <JRogers.dev@gmail.com>
2021-03-08 19:31:12 -0800

    Added code to FoodList to render out fetched foods with a button to add them to the current day

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 
 
 class FoodList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    
+  }
+
   render() {
-    return (
-      <>
-        
-      </>
-    )
+    if (this.props.foods.hits) {
+      return(
+        <div>
+          {this.props.foods.hits.map((food, index) => {
+            return (
+              <div>
+                <p>{food.fields.brand_name} {food.fields.item_name}</p>
+                <p>&emsp;Calories: {food.fields.nf_calories}</p>
+                <p>&emsp;Total Fat: {food.fields.nf_total_fat}g</p>
+                <p>&emsp;Total Carbs: {food.fields.nf_total_carbohydrate}g</p>
+                <p>&emsp;Protein: {food.fields.nf_protein}g</p>
+                <p>&emsp;<button id={food.fields.item_id} onClick={this.handleClick}>Add Food</button></p>
+                <br />
+              </div>
+            )
+          })}
+        </div>
+      );
+    }
+    else {
+      return(
+        <div>
+
+        </div>
+      );
+    }
   }
 }
 
