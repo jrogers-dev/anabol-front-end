77ee4b617d19d0508ca3f489c7d38ae3c2cf6835
jrogers-dev <JRogers.dev@gmail.com>
2021-03-15 01:05:48 -0700

    Added a loading boolean to DayDetail's local state. Also added a massive nested fetch call to see if the date string in the current url has a database entry. If it doesn't, we create one. Then, we store the attributes of the database entry in local state for now. This will be refactored to be more succinct and utulize redux.

--- src/components/DayDetail.js
+++ src/components/DayDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+
 export default class DayDetail extends Component {
   constructor(props) {
     super(props);
@@ -21,28 +22,74 @@ export default class DayDetail extends Component {
   }
 
   componentDidMount() {
-    
+    if (this.props.location.pathname === "/dash/day/today") {
+      let d = new Date();
+
+      fetch(`http://localhost:3000/days/20210316`)
+        .then(response => response.json())
+        .then(json => {
+          if(json.status === 404 && json.exception.includes("RecordNotFound")) {
+            fetch(
+              `http://localhost:3000/days`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json"
+                },
+                body: JSON.stringify({day: {id: 20210316, user_id: 1, date: this.dateString(d, "-")}})
+              }
+            ).then(response => response.json()).then(json => {
+              this.setState({
+                ...this.state,
+                [json.data.attributes.id]: {
+                  user_id: json.data.attributes.user_id,
+                  date: json.data.attributes.date
+                }
+              }, () => {console.log(this.state)})
+            })
+          } 
+          else {
+            this.setState({
+              loading: false,
+              [json.data.attributes.id]: {
+                user_id: json.data.attributes.user_id,
+                date: json.data.attributes.date
+              }
+            }, () => {console.log(this.state)})
+          }
+        })
+        .catch(err => console.log(err))
+      ;
+    }
   }
 
   render() {
-    return (
-      <>
-      <h1>Summary for: </h1>
-      <br />
-      <h3>Protein:</h3>
-      <br />
-      <h3>Carbs:</h3>
-      <br />
-      <h3>Fat:</h3>
-      <br />
-      <h3>Calories:</h3>
-      <hr />
-      <h2>Foods</h2>
-      <br />
-      <h3>*List Foods*</h3>
-      <br />
-      <h3><NavLink to="/dash/days/today/add">Add Food</NavLink></h3>
-      </>
-    )
+    if(this.state.loading === true) {
+      return (
+        <h1>Loading...</h1>
+      )
+    }
+    else {
+      return (
+        <>
+        <h1>Summary for: {this.state[20210316].date}</h1>
+        <br />
+        <h3>Protein:</h3>
+        <br />
+        <h3>Carbs:</h3>
+        <br />
+        <h3>Fat:</h3>
+        <br />
+        <h3>Calories:</h3>
+        <hr />
+        <h2>Foods</h2>
+        <br />
+        <h3>*List Foods*</h3>
+        <br />
+        <h3><NavLink to="/dash/today/add">Add Food</NavLink></h3>
+        </>
+      )
+    }
   }
 }
