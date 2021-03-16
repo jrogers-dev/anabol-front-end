e26af684c97e7183a7350ea374726fdc38c75d85
jrogers-dev <JRogers.dev@gmail.com>
2021-03-15 23:27:01 -0700

    Added functionality to CalendarDetail. It now fetches from the days index of the Rails API, stores the returned days in state and lists them out. These will be used to generate NavLinks.

--- src/components/CalendarDetail.js
+++ src/components/CalendarDetail.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
 export default class CalendarDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      days: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/days`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          loading: false,
+          days: json.data
+        }, () => console.log(this.state))
+      })
+      .catch(err => console.log(err))
+    ;
+  }
 
   render() {
-    return (
-      <>
-       <span>Calendar Detail!</span>
-      </>
-    )
+    if(this.state.loading)
+      return(<h1>Loading...</h1>)
+    else {
+      return (
+        <>
+        {this.state.days.map((day => <p>{new Date(day.attributes.date).toLocaleString().split(',')[0]}</p>))}
+        </>
+      )
+    }
   }
 }
