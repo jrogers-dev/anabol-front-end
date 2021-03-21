2268183d2ec1080e527babce4a22a10b9cb4c90e
James Rogers <jarogers095@gmail.com>
2021-03-21 16:46:57 -0700

    Modified rootReducer so ADD_DAYS adds the days to the store in a format where the date (stripped of hyphens) is used as the key for that day

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,4 @@
-export default function rootReducer(state = { foods: [], days: [], loading: false }, action) {
+export default function rootReducer(state = { foods: [], days: {}, loading: false }, action) {
   switch (action.type) {
     case "LOADING_FOODS":
       return {
@@ -23,9 +23,11 @@ export default function rootReducer(state = { foods: [], days: [], loading: fals
         loading: true
       };
     case "ADD_DAYS":
+      let days_hash = {};
+      action.days.forEach(day => { day_array[day.attributes.id] = day.attributes.date });
       return {
         ...state,
-        days: action.days,
+        days: days_hash,
         loading: false
       };
     case "DELETE_DAY":
